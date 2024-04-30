42badaf14039959e25e112331ad128dd155c3de8
midesofek <midesofek@gmail.com>
2024-04-30 20:45:15 +0100

    added base eth and erc20 controllers

---
+++ controllers/mainnets/arb/ethBalanceController.js
@@ -0,0 +1,27 @@
+const axios = require("axios");
+require("dotenv").config();
+
+// arbitrum ETH //
+
+const ARBISCAN_KEY = process.env.ARBISCAN_API_KEY;
+
+// Function to fetch ETH balance on ARB
+async function ethBalanceController(req, res) {
+  const userAddress = req.params.userAddress;
+
+  try {
+    const response = await axios.get(
+      `https://api.arbiscan.io/api?module=account&action=balance&address=${userAddress}&tag=latest&apikey=${ARBISCAN_KEY}`
+    );
+    const balanceInWei = response.data.result;
+    const balanceInEth = balanceInWei / 1e18; // Convert Wei to Ether
+    console.log(`ARB ETH Balance: ${balanceInEth} ETH`);
+    res.status(200).json(`ARB ETH Balance: ${balanceInEth} ETH`);
+    return balanceInEth;
+  } catch (error) {
+    console.error("Error fetching ETH balance:", error.message);
+    return 0;
+  }
+}
+
+module.exports = ethBalanceController;
