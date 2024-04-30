16eb89d3680ef9e09b36f8cc0f47918d13cd8269
midesofek <midesofek@gmail.com>
2024-04-30 20:45:44 +0100

    added base eth and erc20 controllers

---
+++ controllers/mainnets/base/erc20TokenBalanceController.js
@@ -0,0 +1,28 @@
+const axios = require("axios");
+require("dotenv").config();
+
+// BASE ERC20 tokens //
+
+const BASESCAN_KEY = process.env.BASESCAN_API_KEY;
+
+// Function to fetch any ERC20 token on BASE balance using its ContractAddress
+async function erc20TokenBalanceController(req, res) {
+  const contractAddress = req.params.contractAddress;
+  const userAddress = req.params.userAddress;
+
+  try {
+    const response = await axios.get(
+      `https://api.basescan.io/api?module=account&action=tokenbalance&contractaddress=${contractAddress}&address=${userAddress}&tag=latest&apikey=${BASESCAN_KEY}`
+    );
+    const balanceInWei = response.data.result;
+    const balanceInEth = balanceInWei / 1e18; // Convert Wei to Ether
+    console.log(`Token Balance: ${balanceInEth}`);
+    res.status(200).json(balanceInEth);
+    return balanceInEth;
+  } catch (error) {
+    console.error("Error fetching ETH balance:", error.message);
+    return 0;
+  }
+}
+
+module.exports = erc20TokenBalanceController;
---
+++ controllers/mainnets/base/ethBalanceController.js
@@ -0,0 +1,27 @@
+const axios = require("axios");
+require("dotenv").config();
+
+// BASE ETH BALANCE//
+
+const BASESCAN_KEY = process.env.BASESCAN_API_KEY;
+
+// Function to fetch ETH balance on ARB
+async function ethBalanceController(req, res) {
+  const userAddress = req.params.userAddress;
+
+  try {
+    const response = await axios.get(
+      `https://api.basescan.io/api?module=account&action=balance&address=${userAddress}&tag=latest&apikey=${BASESCAN_KEY}`
+    );
+    const balanceInWei = response.data.result;
+    const balanceInEth = balanceInWei / 1e18; // Convert Wei to Ether
+    console.log(`BASE ETH Balance: ${balanceInEth} ETH`);
+    res.status(200).json(`BASE ETH Balance: ${balanceInEth} ETH`);
+    return balanceInEth;
+  } catch (error) {
+    console.error("Error fetching BASE ETH balance:", error.message);
+    return 0;
+  }
+}
+
+module.exports = ethBalanceController;
