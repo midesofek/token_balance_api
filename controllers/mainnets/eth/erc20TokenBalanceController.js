0196492f23875c7f28f26af1adb708482bc3fd4e
midesofek <midesofek@gmail.com>
2024-05-01 09:54:05 +0100

    fix: token decimals bug

--- controllers/mainnets/eth/erc20TokenBalanceController.js
+++ controllers/mainnets/eth/erc20TokenBalanceController.js
@@ -1,18 +1,33 @@
+const ethers = require("ethers");
 const axios = require("axios");
 require("dotenv").config();
 
-const ETHERSCAN_KEY = process.env.ETHERSCAN_API_KEY;
+const { ETHERSCAN_API_KEY: ETHERSCAN_KEY, ETH_RPC_URL } = process.env;
+
+async function fetchDecimals(tokenAddress) {
+  const provider = new ethers.providers.JsonRpcProvider(ETH_RPC_URL);
+  const res = await axios.get(`
+    https://api.etherscan.io/api?module=contract&action=getabi&address=${tokenAddress}&apikey=${ETHERSCAN_KEY}`);
+  const abi = res.data.result;
+  console.log(abi);
+  const tokenContract = new ethers.Contract(tokenAddress, abi, provider);
+  const decimals = await tokenContract.decimals();
+  console.log(decimals);
+  return decimals;
+}
 
 // Function to fetch any ERC20 token balance using its ContractAddress
 async function erc20TokenBalanceController(req, res) {
   const { userAddress, contractAddress } = req.body;
 
   try {
+    const decimals = await fetchDecimals(contractAddress);
+
     const response = await axios.get(
       `https://api.etherscan.io/api?module=account&action=tokenbalance&contractaddress=${contractAddress}&address=${userAddress}&tag=latest&apikey=${ETHERSCAN_KEY}`
     );
     const balanceInWei = response.data.result;
-    const balanceInEth = balanceInWei / 1e18; // Convert Wei to Ether
+    const balanceInEth = balanceInWei / 10 ** decimals; // Convert Wei to Ether
     console.log(`Token Balance: ${balanceInEth}`);
     res.status(200).json(balanceInEth);
     return balanceInEth;
